fd5fd16c330c4e9f1f45b7a91afbad55211dc614
Giuseppe Tiberi <tiberiweb@gmail.com>
2016-03-13 16:52:20 +0000

    Added Lists and Funtions test file

---
+++ List_and_Functions.py
@@ -0,0 +1,28 @@
+def count_small(numbers):
+    total = 0
+    for n in numbers:
+        if n < 10:
+            total = total + 1
+    return total
+
+lost = [4,8,16,17,23,45]
+
+small = count_small(lost)
+
+print "The result of the 'count_small' function is: " + str(small)
+
+print ('')
+print ('String Looping: ')
+print ('')
+
+for letter in "Supermarket":
+    print letter
+
+print ('')
+
+sentence = "Coding is fun!"
+
+for letter in sentence:
+     # Only print out the letter i
+     if letter == "i":
+         print letter
