e1f3c253d982a1224edfb8069f5678b8c67f0684
Giuseppe Tiberi <tiberiweb@gmail.com>
2016-03-13 19:03:19 +0000

    Added compute_bill() function for the customer shopping

--- supermarket.py
+++ supermarket.py
@@ -12,8 +12,35 @@ stock = {
     "pear": 15
 }
 
+print ("The Supermarket prices and stock are: " + "\n")
+
 for key in prices:
     print key
     print "price: %s " % prices[key]
     print "stock: %s " % stock[key]
     print " "
+
+print (' ')
+
+total = 0
+
+print ("The Supermarket stock values is: " + "\n")
+
+for key in prices:
+    total += prices[key] * stock[key]
+    print key + " : " + str(total) + " Euro"
+
+print (' ')
+
+# Shopping at the Market
+
+shopping_list = ["banana", "orange", "apple"]
+
+
+def compute_bill(food):
+    total_bill = 0
+    for item in food:
+        if stock[item] > 0:
+            total_bill += prices[item]
+            stock[item] = stock[item] - 1
+    return total_bill
